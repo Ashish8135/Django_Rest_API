2f7a21e6bf56bdc878e3b8f3a86b43d6c9bb7112
ashish8135 <ashish.shaivya10@gmail.com>
2021-01-23 21:56:02 +0530

    Created fetched api

--- CRUD_API_DRF/crud_api/views.py
+++ CRUD_API_DRF/crud_api/views.py
@@ -9,20 +9,25 @@ from django.http import HttpResponse
 # Create your views here.
 
 def student_api(request):
-    if request.method=="GET":
-        json_data=request.body 
-        stream = io.BytesIO(json_data)   
-        python_data =JSONParser().parse(stream)  
+    if request.method=="GET":   # get/fetch data from database.
+        json_data=request.body     # 
+        stream = io.BytesIO(json_data)    
+        # parse json data to python native data type
+        python_data =JSONParser().parse(stream)   #used to parse json data to python native data type
+        # store python data into variable
         id= python_data.get('id',None)
+        # if data is present then fetch   
         if id is not None:
-            stu=Student.objects.get(id=id)
-            serializer=StudentSerializer(stu)
-            json_data=JSONRenderer().render(serializer.data)
-            return HttpResponse(json_data,content_type='application/json')
+            stu=Student.objects.get(id=id)  # fetching single data from database
+            #complex to python data type 
+            serializer=StudentSerializer(stu) #complex to python data type
+            json_data=JSONRenderer().render(serializer.data)  # python data type to json
+            return HttpResponse(json_data,content_type='application/json') 
 
-        stu=Student.objects.all()
-        serializer=StudentSerializer(stu,many=True)
-        json_data=JSONRenderer().render(serializer.data)
+        stu=Student.objects.all()  # fetch all data from database
+        # complex to python data type
+        serializer=StudentSerializer(stu,many=True)  # complex to python data type
+        json_data=JSONRenderer().render(serializer.data) # python data type to json
         return HttpResponse(json_data,content_type='application/json')
 
 
