f18a8b36848007514df4e37a5265ce7f21431f3d
ashish8135 <ashish.shaivya10@gmail.com>
2021-02-12 02:14:45 +0530

    crudapi

--- CrudApiView/crudapi/views.py
+++ CrudApiView/crudapi/views.py
@@ -7,5 +7,15 @@ from .serializer import StudentSerializer
 # Create your views here.
 
 @api_view(['GET','POST','PUT','DELETE'])
+def studentapi(request):
+    if request.method=='GET':
+        id=request.data.GET('id')
+        if id is not None:
+            stu=Student.objects.GET(id=id)
+            serialzer=StudentSerializer(stu)
+            return Response(serialzer.data)
+        stu=Student.objects.all()
+        serialzer=StudentSerializer(stu,many=True)
+        return Response(serialzer.data)
 
 
