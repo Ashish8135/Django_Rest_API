dfe4b99aa18f43fc25565877d54c4b995cc448ac
ashish8135 <ashish.shaivya10@gmail.com>
2021-02-15 00:50:06 +0530

    Mixin completed

--- Mixin/Mixin/urls.py
+++ Mixin/Mixin/urls.py
@@ -19,5 +19,9 @@ from crudapi import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('stuapi/',views.StudentList.as_view()),
+    # path('stuapi/',views.StudentCreate.as_view()),
+    # path('stuapi/<int:pk>/',views.StudentRetrieve.as_view()),
+    # path('stuapi/<int:pk>/',views.StudentUpdate.as_view()),
+    # path('stuapi/<int:pk>/',views.StudentDestroy.as_view()),
     # path('stuapi/<int:pk>/',views.StudentList.as_view()),
 ]
