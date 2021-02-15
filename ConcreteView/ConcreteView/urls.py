f25c62ecb419efda08b8bae96e38cebbeb4f88b0
ashish8135 <ashish.shaivya10@gmail.com>
2021-02-15 23:39:46 +0530

    concrete view

--- ConcreteView/ConcreteView/urls.py
+++ ConcreteView/ConcreteView/urls.py
@@ -15,7 +15,19 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from crudapi import views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # path('stuapi/',views.StudentList.as_view()),
+    # path('stuapi/',views.StudentCreate.as_view()),
+    # path('stuapi/<int:pk>/',views.StudentUpdate.as_view()),
+    # path('stuapi/<int:pk>/',views.StudentRetrieve.as_view()),
+    # path('stuapi/<int:pk>/',views.Studentdelete.as_view()),
+    # path('stuapi/',views.StudentGetPost.as_view()),
+    # path('stuapi/<int:pk>/',views.StudentPutDel.as_view()),
+     path('stuapi/',views.StudentListCreate.as_view()),
+     path('stuapi/<int:pk>/',views.StudentRetrieveUpdate.as_view()),
+     path('stuapi/<int:pk>/',views.StudentRetrieveDestroy.as_view()),
+     path('stuapi/<int:pk>/',views.StudentRetrieveUpdateDestroy.as_view()),
+
 ]
