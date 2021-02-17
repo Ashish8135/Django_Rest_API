22ccbb9129da8b3e7d2c5436e8f87734a7e59155
ashish8135 <ashish.shaivya10@gmail.com>
2021-02-17 19:46:59 +0530

    Session Authentication and Permission

--- SessionAuthentication/SessionType/views.py
+++ SessionAuthentication/SessionType/views.py
@@ -4,7 +4,7 @@ from rest_framework.response import Response
 from rest_framework import viewsets
 from .serializer import StudentSerializer
 from rest_framework.authentication import SessionAuthentication
-from rest_framework.permissions import IsAuthenticated,AllowAny,IsAdminUser,IsAuthenticatedOrReadOnly,DjangoModelPermissions
+from rest_framework.permissions import IsAuthenticated,AllowAny,IsAdminUser,IsAuthenticatedOrReadOnly,DjangoModelPermissions,DjangoModelPermissionsOrAnonReadOnly
 # Create your views here.
 
 class StudentSessionAuthentication(viewsets.ModelViewSet):
@@ -15,4 +15,5 @@ class StudentSessionAuthentication(viewsets.ModelViewSet):
     # permission_classes=[AllowAny]
     # permission_classes=[IsAdminUser]
     # permission_classes=[IsAuthenticatedOrReadOnly]
-    permission_classes=[DjangoModelPermissions]
+    # permission_classes=[DjangoModelPermissions]
+    permission_classes=[DjangoModelPermissionsOrAnonReadOnly]
