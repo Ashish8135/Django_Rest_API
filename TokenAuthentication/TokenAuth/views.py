9aeab077a6c568bd3e35b06ce0d86174431b5e43
ashish8135 <ashish.shaivya10@gmail.com>
2021-02-22 00:42:58 +0530

    TokenAuthentication and permission

--- TokenAuthentication/TokenAuth/views.py
+++ TokenAuthentication/TokenAuth/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .serializer import StudentSerializer
 from .models import Student
 from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
 # Create your views here.
 
 class StudentModelViewSet(viewsets.ModelViewSet):
