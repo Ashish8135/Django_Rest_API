4409231756a4335dbfb72238c673ec2d408cc0e7
ashish8135 <ashish.shaivya10@gmail.com>
2021-03-18 18:19:19 +0530

    LimitoffsetPagination and cursorPagination

--- LimitOffsetPagination/limitoffset/pagination.py
+++ LimitOffsetPagination/limitoffset/pagination.py
@@ -1,7 +1,12 @@
-from rest_framework.pagination import LimitOffsetPagination
+from rest_framework.pagination import LimitOffsetPagination,CursorPagination
 
-class MyLimitPagination(LimitOffsetPagination):
-    default_limit=5
-    limit_query_param='limit'
-    offset_query_param='myoffset'
-    max_limit=4
+# class MyLimitPagination(LimitOffsetPagination):
+#     default_limit=5
+#     limit_query_param='limit'
+#     offset_query_param='myoffset'
+#     max_limit=4
+
+class MyCursorPagination(CursorPagination):
+    page_size=2
+    ordering='name'
+    
--- LimitOffsetPagination/limitoffset/views.py
+++ LimitOffsetPagination/limitoffset/views.py
@@ -3,9 +3,10 @@ from .serializer import StudentSerializer
 from .models import Student
 from rest_framework.generics import ListAPIView
 from rest_framework.pagination import LimitOffsetPagination
-from .pagination import MyLimitPagination
+from .pagination import MyCursorPagination
 # Create your views here.
 class StudentList(ListAPIView):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
-    pagination_class=MyLimitPagination
+    # pagination_class=MyLimitPagination
+    pagination_class=MyCursorPagination
